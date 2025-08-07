3820abc33e0b1f57f372842e9992414c8cce342e
gabrvp <gabrusevich.valeriy1997@gmail.com>
2025-08-07 23:24:00 +0700

    fixed Json.php

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Differ\Formatters\Json;
 
 function jsonFormat(array $diff): string
 {
-    return json_encode($diff, JSON_PRETTY_PRINT);
+    $result = json_encode($diff, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
+    return $result;
 }
